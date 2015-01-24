721d6fd617f88b85dedfe76e66c04e22abdb206b
Ryan Clarke <ryan@ryanclarke.net>
2015-01-24 07:41:25 -0500

    Working MVP

---
+++ TalkTimer/TalkTimer.Shared/Clock.cs
@@ -0,0 +1,68 @@
+﻿namespace TalkTimer
+{
+    public class Clock
+    {
+        private const int SecondsPerMinute = 60;
+
+        private int _seconds;
+
+        public int Raw { get { return _seconds; } }
+        public string Minutes { get { return ToWholeMinutes(_seconds).ToString("0"); } }
+        public string Seconds { get { return WithWholeMinutesRemoved(_seconds).ToString("00"); } }
+
+        public Clock(int minutes, int seconds = 0)
+        {
+            Set(minutes, seconds);
+        }
+
+        public void Set(int minutes, int seconds = 0)
+        {
+            _seconds = ToRaw(minutes, seconds);
+        }
+
+        public void ElapseSecond()
+        {
+            _seconds--;
+        }
+
+        public bool IsAt(int minutes, int seconds = 0)
+        {
+            return Raw == ToRaw(minutes, seconds);
+        }
+
+        public bool JustPassed(int minutes, int seconds = 0)
+        {
+            return Raw == ToRaw(minutes, seconds) - 1;
+        }
+
+        internal bool IsInOvertime()
+        {
+            return Raw <= 0;
+        }
+
+        internal bool IsLastMinute()
+        {
+            return Raw < SecondsPerMinute && Raw > 0;
+        }
+
+        private static int ToRaw(int minutes, int seconds = 0)
+        {
+            return ToSeconds(minutes) + seconds;
+        }
+
+        private static int ToSeconds(int minutes)
+        {
+            return minutes * SecondsPerMinute;
+        }
+
+        private static int ToWholeMinutes(int seconds)
+        {
+            return seconds / SecondsPerMinute;
+        }
+
+        private static int WithWholeMinutesRemoved(int seconds)
+        {
+            return seconds % SecondsPerMinute;
+        }
+    }
+}
---
+++ TalkTimer/TalkTimer.Shared/ColorTimerControl.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace TalkTimer
+{
+    public sealed partial class ColorTimerControl : UserControl
+    {
+        private const int _minutes = 10;
+        private Clock _clock;
+        private DispatcherTimer _timer;
+
+        public ColorTimerControl()
+        {
+            this.InitializeComponent();
+
+            _clock = new Clock(_minutes);
+            _timer = new DispatcherTimer();
+            _timer.Interval = new TimeSpan(0, 0, 1);
+            _timer.Tick += timer_Tick;
+
+            UpdateCounterUI();
+        }
+
+        private void ColorBox_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (_clock.IsInOvertime())
+            {
+                _timer.Stop();
+                _clock.Set(_minutes);
+                UpdateCounterUI();
+            }
+            else if (_clock.IsAt(_minutes))
+            {
+                StartTimer();
+            }
+        }
+
+        private void StartTimer()
+        {
+            _clock.Set(_minutes);
+
+            _timer.Start();
+
+            UpdateCounterUI();
+        }
+
+        void timer_Tick(object sender, object e)
+        {
+            _clock.ElapseSecond();
+            UpdateCounterUI();
+        }
+
+        private void UpdateCounterUI()
+        {
+            var clockColor = _timer.IsEnabled ? Colors.White : Colors.DarkSlateGray;
+            LargeNumber.Foreground = new SolidColorBrush(clockColor);
+            SmallNumber.Foreground = new SolidColorBrush(clockColor);
+
+            if (_clock.IsAt(_minutes))
+            {
+                var backgroundColor = _timer.IsEnabled ? Colors.Green : Colors.Black;
+                ColorBox.Background = new SolidColorBrush(backgroundColor);
+            }
+            else if (_clock.JustPassed(2))
+            {
+                ColorBox.Background = new SolidColorBrush(Colors.Purple);
+            }
+            else if (_clock.JustPassed(1))
+            {
+                ColorBox.Background = new SolidColorBrush(Colors.Red);
+            }
+            else if (_clock.IsAt(0))
+            {
+                ColorBox.Background = new SolidColorBrush(Colors.Black);
+            }
+
+            if (_clock.IsLastMinute())
+            {
+                LargeNumber.Text = _clock.Seconds;
+                SmallNumber.Text = "";
+            }
+            else if (_clock.IsInOvertime())
+            {
+                LargeNumber.Text = _clock.Minutes;
+                SmallNumber.Text = "";
+            }
+            else
+            {
+                LargeNumber.Text = _clock.Minutes;
+                SmallNumber.Text = _clock.Seconds;
+            }
+        }
+    }
+}
